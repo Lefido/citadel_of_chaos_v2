1c805487e1b1343e133c23787ba4179a9250f027
Lefido <lefido@hotmail.com>
2023-02-08 14:59:54 +0100

    Rotation chiffre du dé

--- script/home.js
+++ script/home.js
@@ -1,11 +1,21 @@
 
+import msgBox from "./msgbox.js"
+
 const start_game = document.querySelector('.start-game')
 const new_game = document.querySelector('.new-game')
 
 
 start_game.addEventListener("click", function() {
 
-    window.location.href= './general.html'
+   
+
+    msgBox("header", "Chargement de la partie", "Récupération de vos paramètres...")
+
+    setTimeout(function() {
+        window.location.href= './general.html'
+    }, 2000)
+
+
 
 })
 
@@ -13,6 +23,12 @@ new_game.addEventListener("click", function() {
 
     console.log("J'ai cliqué sur start")
 
-    window.location.href= './player.html'
+    msgBox("header", "Lancement du nouvelle partie", "Chargement des paramètres...")
+
+    setTimeout(function() {
+        window.location.href= './player.html'
+    }, 2000)
+
+   
 
 })
