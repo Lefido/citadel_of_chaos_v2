e6c177ed9f0ce547287551314c88978017702a08
Lefido <lefido@hotmail.com>
2023-02-09 21:53:27 +0100

    Maj services

--- script/login.js
+++ script/login.js
@@ -1,42 +1,49 @@
 
 
 
-import {loginUser, updateToken, getUser, delToken } from "./function.js"
+import {loginUser,
+    updateToken,
+    getUser,
+    delToken }
+    from "./function.js";
 
 // delToken()
 
-let token = updateToken()
+let token = updateToken();
 
 if (token != "") {
 
-    let tokenParse = JSON.parse(token)
+    let tokenParse = JSON.parse(token);
 
-    console.log("Transformation du token en format text en JSON" ,tokenParse)
+    console.log("Transformation du token en format text en JSON");
 
-    // getUser(tokenParse)
+    console.log(tokenParse);
+
+
+    getUser(tokenParse);
 
 }
 
 
 
-let userName = document.querySelector('#name')
-let userPsw = document.querySelector('#psw')
-let btn_connexion = document.querySelector('#valider')
-let header = document.querySelector('header')
+let userName = document.querySelector('#name');
+let userPsw = document.querySelector('#psw');
+let btn_connexion = document.querySelector('#valider');
+let header = document.querySelector('header');
 
 btn_connexion.addEventListener('click', function(){
 
     if (userName.value == "") {
 
-        console.log("Veuillez saisir un utilisateur")
-        msgBox("header", "Information", "Veuillez saisir un utilisateur")
-        return
+        console.log("Veuillez saisir un utilisateur");
+        msgBox("header", "Information", "Veuillez saisir un utilisateur");
+        return;
     } 
 
     if (userPsw.value === "") {
 
-        console.log("Veuillez saisir votre mot de passe")
-        msgBox("header", "Information", "Veuillez saisir votre mot de passe")
+        console.log("Veuillez saisir votre mot de passe");
+        msgBox("header", "Information", "Veuillez saisir votre mot de passe");
         return
 
     }
@@ -46,7 +53,7 @@ btn_connexion.addEventListener('click', function(){
         "password" : userPsw.value
     }
 
-    loginUser(userData)
+    loginUser(userData);
 
 
     
